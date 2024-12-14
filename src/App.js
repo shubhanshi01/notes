87cbfacf3c3db23ef379b6594e5f7f758fdcad3c
shubhanshi0101 <shubhanshi743052@gmail.com>
2024-12-14 20:41:50 +0530

    lets see

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">FunNote</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="/" tabIndex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
+  </div>
+</nav>
+<div className="main-card">
+<div className="card" style={{"width":"18rem","backgroundColor":"yellow"}}>
+  <div className="card-body">
+    <h5 className="card-title">KEEPER</h5>
+    <h6 className="card-subtitle mb-2 text-muted">Add a note </h6>
+    <form className="note">
+      <input className="text" type="text" ></input>
+    </form>
+    <a href="/" className="card-link">Card link</a>
+    <a href="/" className="card-link">Another link</a>
+  </div>
+</div>
+</div>
+</div>
   );
 }
 
